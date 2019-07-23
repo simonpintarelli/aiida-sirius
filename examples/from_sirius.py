a24d911cee46937ba7d1308ce3732a83b266dcb6
Simon Pintarelli <simon.pintarelli@cscs.ch>
2019-07-23 21:51:13 +0200

    add files

---
+++ examples/from_sirius.py
@@ -0,0 +1,70 @@
+from aiida.orm import StructureData, Dict
+from aiida.common.constants import bohr_to_ang
+import numpy as np
+
+
+def to_list(x):
+    return [list(xi) for xi in x]
+
+
+def read_magnetization(unit_cell_atoms):
+    def get_mag(pos_mag):
+        """
+        returns [0, 0, 0] if not specified
+        """
+        mag = pos_mag[3:]
+        if np.size(mag) == 0:
+            return np.array([0, 0, 0])
+        else:
+            return mag
+    magnetization = {}
+    for atom_type in unit_cell_atoms:
+        mag = [get_mag(lpos) for lpos in unit_cell_atoms[atom_type]]
+        magnetization[atom_type] = mag
+    return magnetization
+
+
+def structure_mag_from_sirius_json(sirius_json):
+    """Create input provenance from sirius_json. Assuming quantities
+    are given in atomic units!
+
+    Returns structure (unit_cell, sites) and magnetization
+
+    Keyword Arguments:
+    sirius_json -- sirius_json (as dict)
+
+    Returns:
+    - StructureData
+    - magnetization dictionary
+
+    """
+    sirius_unit_cell = sirius_json['unit_cell']
+
+    if 'lattice_vectors_scale' in sirius_unit_cell:
+        lattice_vectors_scale = sirius_unit_cell['lattice_vectors_scale']
+    else:
+        lattice_vectors_scale = 1
+
+    cell_angstrom = np.array(sirius_unit_cell['lattice_vectors']) * lattice_vectors_scale * bohr_to_ang
+
+    magnetization = read_magnetization(sirius_unit_cell['atoms'])
+
+    s = StructureData(cell=to_list(cell_angstrom))
+    if 'atom_coordinate_units' in sirius_unit_cell:
+        if sirius_unit_cell['atom_coordinate_units'] in ['A', 'a.u.', 'au']:
+            # atom positions are already given in angstrom, nothing to convert
+            for atom_type in sirius_unit_cell['atoms']:
+                for lposmag in sirius_unit_cell['atoms'][atom_type]:
+                    lpos = np.array(lposmag[:3])
+                    s.append_atom(position=tuple(lpos), symbols=atom_type)
+        else:
+            raise ValueError('invalid entry for atom_coordinate_units')
+    else:
+        # atom positions are given in relative coordinates
+        for atom_type in sirius_unit_cell['atoms']:
+            for lposmag in sirius_unit_cell['atoms'][atom_type]:
+                lpos = np.array(lposmag[:3])
+                apos = np.dot(cell_angstrom.T, lpos)
+                s.append_atom(position=tuple(apos), symbols=atom_type)
+
+    return s, magnetization
---
+++ examples/submit_nlcg_from_sirius_json.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*-
+"""Submit a test calculation on daint-gpu
+
+Usage: verdi run submit.py
+"""
+
+from __future__ import absolute_import
+from __future__ import print_function
+import os
+from aiida_sirius import tests, helpers
+from aiida.plugins import DataFactory, CalculationFactory
+from aiida.engine import run, submit
+from aiida.orm.nodes.data.upf import get_pseudos_from_structure
+from aiida.orm import Code
+import json
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--computer', '-c', default='daint-gpu')
+args = parser.parse_args()
+
+# converters sirius_json to aiida provenance input
+from from_sirius import structure_mag_from_sirius_json
+
+sirius_json = json.load(open('sirius.json', 'r'))
+
+# get code
+computer = helpers.get_computer(args.computer)
+# code = helpers.get_code(entry_point='sirius.nlcg', computer=computer)
+code = Code.get_from_string('sirius.nlcg@' + computer.get_name())
+
+params = {
+        "electronic_structure_method": "pseudopotential",
+        "xc_functionals": ["XC_GGA_X_PBE", "XC_GGA_C_PBE"],
+        "smearing_width": 0.025,
+        "use_symmetry": True,
+        "num_mag_dims": 1,
+        "gk_cutoff": 6.0,
+        "pw_cutoff": 27.00,
+        "num_dft_iter": 2
+}
+
+####################
+# # Prepare inputs #
+####################
+SiriusParameters = DataFactory('sirius.scf')
+StructureData = DataFactory('structure')
+KpointsData = DataFactory('array.kpoints')
+Dict = DataFactory('dict')
+parameters = SiriusParameters({'control': {},
+                               'iterative_solver': {},
+                               'parameters': params,
+                               'mixer': {}})
+SinglefileData = DataFactory('singlefile')
+
+NLCGParameters = DataFactory('sirius.nlcg')
+nlcg_marzari = {'type': 'Marzari', 'inner': 2, 'fd_slope_check': False}
+precond = {'type': 'kinetic', 'eps': 0.001}
+nlcgconfig = {
+    "CG": {
+        "method": nlcg_marzari,
+        "type": "FR",
+        "maxiter": 5,
+        "restart": 20,
+        "tau": 0.1,
+        "nscf": 1,
+        "precond": precond,
+        "tol": 1e-09,
+    },
+    "System": {"T": 300.0, "smearing": "gaussian-spline"},
+}
+nlcgparams = NLCGParameters(nlcgconfig)
+
+structure, magnetization = structure_mag_from_sirius_json(sirius_json)
+
+####################################################
+# # Warning pseudopotentials are taken from aiida! #
+####################################################
+pseudos = get_pseudos_from_structure(structure, 'normcons')
+
+kpoints = KpointsData()
+kpoints.set_kpoints_mesh([2, 2, 2])
+
+# 'num_cores_per_machine': 1,
+comp_resources = {'num_mpiprocs_per_machine': 2,
+                  'num_machines': 4,
+                  'num_cores_per_mpiproc': 6}
+
+# set up calculation
+inputs = {
+    'code': code,
+    'sirius_config': parameters,
+    'structure': structure,
+    'kpoints': kpoints,
+    'nlcgparams': nlcgparams,
+    'magnetization': Dict(dict=magnetization),
+    'metadata': {
+        'description': "Test job submission with the aiida_sirius plugin",
+        'options': {
+            'resources': comp_resources,
+            'withmpi': True,
+            'max_wallclock_seconds': 200
+        }
+    },
+    'pseudos': pseudos
+}
+
+# Note: in order to submit your calculation to the aiida daemon, do:
+# from aiida.engine import submit
+# future = submit(CalculationFactory('sirius'), **inputs)
+calc = CalculationFactory('sirius.nlcg')
+result = submit(calc, **inputs)
